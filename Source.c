a1642714c18a8f04584824bc31f21c2d7a4e095f
rebeca-julia <111317728+rebeca-julia@users.noreply.github.com>
2023-08-18 09:01:43 -0300

    Add files via upload

---
+++ Source.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+	int i = 0;
+	i = 0;
+	switch (i)
+	{
+	case 0:
+			printf("zero\n");
+	case 1:
+		default:
+		printf("nao achou");
+		break;
+	}
+	return 0;
+}
---
+++ SwitchDoWhile.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+	int i = 0;
+	i = 0;
+	do
+	{
+		printf("AloMundo\n");
+		i++;
+	} 
+	while (i > 3);
+	return 0; 
+}
